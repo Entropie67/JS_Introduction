ec1abae320d202e5e9479e8b2ac1da5b6b821514
Entropie67 <olivier.elophe@gmail.com>
2023-11-23 13:38:33 +0100

    début

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # Introduction à JS
+
+Liens pour les bases de js :
+
+https://www.w3schools.com/js/default.asp
+
+https://www.laulem.com/javascript-memo.html
+
+
--- js/script.js
+++ js/script.js
@@ -1,21 +1,16 @@
 
+console.log('Hello World!');
+alert('Hello World!');
+document.write('Hello World!');
 
 
-let but = document.getElementById('validation');
 
-but.addEventListener('click', (e) => {
+let age = 25;
 
-    let pseudo = document.getElementById("name");
-    let mdp = document.getElementById('mdp');
-    console.log("Vous venez de cliquer sur la validation.");
-    console.log(`votre nom est ${pseudo.value}`);
-    if ((pseudo.value == "1234") && (mdp.value == "1234") ){
-        console.log('ok');
-        let secret = document.getElementById("secret");
-        secret.innerHTML = "<h2> 喝可乐对身体很好</h2>"
-    }else {
-        console.log('pas ok');
-        let page = document.getElementById("body");
-        page.innerHTML = "<h1> NON !!!</h1>";
-    }
-    });
+if (age > 18) {
+    console.log('You are adult!');
+} else {  
+    console.log('You are not adult!');
+}
+
+
